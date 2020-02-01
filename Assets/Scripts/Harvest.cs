d261f06ca43c26aa0e2f4b6ac37588282e1e7744
Ben Nilsson <55544010+BenNilsson@users.noreply.github.com>
2020-02-01 14:51:11 +0000

    Resources Regrow

--- Assets/Scripts/Harvest.cs
+++ Assets/Scripts/Harvest.cs
@@ -7,19 +7,55 @@ public class Harvest : MonoBehaviour, IHarvestable
     public int health = 3;
     public Vector2 amountToDrop = new Vector2(1,3);
     public GameObject itemToDropPrefab;
+    public Vector2 respawnTime = new Vector2(5, 15);
+    public List<Sprite> sprites = new List<Sprite>();
+    public SpriteRenderer spriteRenderer;
+    public bool harvestable;
+
+    private Sprite randomSprite;
+    private int initialHealth;
+
+    private float timeElapsed;
+    private float randomRespawnTime;
+
+    void Start()
+    {
+        initialHealth = health;
+        randomRespawnTime = Random.Range(respawnTime.x, respawnTime.y);
+        randomSprite = sprites[Random.Range(1, sprites.Count)];
+        spriteRenderer.sprite = randomSprite;
+    }
 
     public void TakeDamage(int amount)
     {
-        health -= amount;
-        if (health <= 0)
-            HarvestItem();
+        if(harvestable)
+        {
+            health -= amount;
+            if (health <= 0)
+                HarvestItem();
+        }
+    }
+
+    void Update()
+    {
+        if(Time.time >= timeElapsed + randomRespawnTime && !harvestable)
+        {
+            timeElapsed = Time.time;
+            harvestable = true;
+            GetComponent<Collider2D>().enabled = true;
+            spriteRenderer.sprite = randomSprite;
+            health = initialHealth;
+        }
     }
 
     public void HarvestItem()
     {
         Debug.Log("Harvested item " + gameObject.name);
         DropItem();
-        Destroy(gameObject);
+        harvestable = false;
+        GetComponent<Collider2D>().enabled = false;
+        spriteRenderer.sprite = sprites[0];
+        timeElapsed = Time.time;
     }
 
     public void DropItem()
