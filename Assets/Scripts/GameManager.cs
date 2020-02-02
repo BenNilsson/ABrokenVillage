d1e65f5f728bb4dcad4f1fb40f0e727b1435358c
Ben Nilsson <55544010+BenNilsson@users.noreply.github.com>
2020-02-02 11:51:28 +0000

    Win Condition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -10,7 +11,13 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private GameObject deathMenu;
     [SerializeField] private GameObject inventoryMenu;
+    [SerializeField] private TextMeshProUGUI timer;
     public Transform houseParent;
+
+    private float timeSinceLevelLoaded;
+
+    private float minutes;
+    private float seconds;
 
     public List<House> houses = new List<House>();
 
@@ -25,10 +32,19 @@ public class GameManager : MonoBehaviour
     private void OnEnable()
     {
         AddHousesToList();
+        timeSinceLevelLoaded = Time.deltaTime;
     }
 
     private void Update()
     {
+        UpdateTimer();
+
+        if(CheckWin())
+        {
+            Debug.Log("YOU WON!!!!");
+            Time.timeScale = 0;
+        }
+
         CheckHouseRepairable();
         
         if(!PlayerManager.instance.isAlive)
@@ -37,6 +53,37 @@ public class GameManager : MonoBehaviour
             deathMenu.SetActive(true);
             inventoryMenu.SetActive(false);
         }
+    }
+
+    private bool CheckWin()
+    {
+        if (timeSinceLevelLoaded >= 300)
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    private void UpdateTimer()
+    {
+        timeSinceLevelLoaded += Time.deltaTime;
+
+        minutes = Mathf.Floor(timeSinceLevelLoaded / 60);
+        seconds = Mathf.RoundToInt(timeSinceLevelLoaded % 60);
+
+        string min = "";
+        string sec = "";
+
+        if (minutes != 0)
+            min = minutes.ToString();
+        if (seconds < 10)
+            sec = "0" + Mathf.RoundToInt(seconds).ToString();
+        else sec = Mathf.RoundToInt(seconds).ToString();
+
+
+        if (minutes != 0)
+            timer.text = min + ":" + sec;
+        else timer.text = sec;
     }
 
     private void CheckHouseRepairable()
